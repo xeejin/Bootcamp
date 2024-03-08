148033a3a38a9b5bc33eea6763e1fe7668d0c90b
xeejin <heezeenee@gmail.com>
2024-03-08 11:46:58 +0900

    계산기 구현 시작

--- Calculator/app/src/main/java/com/limheejin/calculator/Calculator.kt
+++ Calculator/app/src/main/java/com/limheejin/calculator/Calculator.kt
@@ -1,20 +1,26 @@
 package com.limheejin.calculator
 
 
-class Calculator {
 
     fun main() {
 
-        println("안녕하세요! 계산기입니다. ")
-        var num1 = readLine()!!.toInt()
-        var num2 = readLine()!!.toInt()
+        println("=============계산기=============")
+        println("안녕하세요! 사칙연산 계산기입니다.")
+        println("계산을 원하시는 정수 두 개를 입력해주세요.")
+        var num1 = Integer.parseInt(readLine())
+        var num2 = Integer.parseInt(readLine())
 
-        // sum이라는 이름의 메소드를 호출!
-        sum(num1, num2)
-    }
+        println("원하시는 연산자를 선택해주세요 (+, -, *, /, %")
+        val operator: String? = readLine()
+        val result =
+            when (operator){
+                "+" -> num1 + num2
+                "-" -> num1 - num2
+                "*" -> num1 * num2
+                "/" -> num1 / num2
+                else -> throw Exception("잘못된 연산자입니다. 계산기를 다시 켜주세요.")
+            }
 
-    fun sum(num1: Int, num2: Int) {
-        var result = num1 + num2
-        println("num1과 num2의 덧셈결과는 ${result}입니다.")
+        println("${num1} ${operator} ${num2} = ${result} 입니다.")
+
     }
-}
